90dc6b790c1dbaf92232e74e29ba31389e8cd0fa
Mario Fink <mario.fink@goodyear.com>
2018-07-25 21:58:00 +0200

    calculate colour combinations with tinycolor2

--- src/components/FarbenSpiel.vue
+++ src/components/FarbenSpiel.vue
@@ -4,18 +4,47 @@
         Base:
         <input type="color" v-model="baseColor">
         {{baseColor}}
+
+        <ul>
+            <li>{{combinations.analogous}}</li>
+            <li>{{combinations.monochromatic}}</li>
+            <li>{{combinations.tetrad}}</li>
+            <li>{{combinations.splitcomplement}}</li>
+            <li>{{combinations.triad}}</li>
+        </ul>
     </div>
 </template>
 
 <script>
+const tinycolor = require("tinycolor2");
+
 export default {
   name: "FarbenSpiel",
   data: () => {
     return {
       baseColor: "#c0ffee"
     };
+  },
+  computed: {
+    combinations: function() {
+      return {
+        analogous: getHexColors(tinycolor(this.baseColor).analogous()),
+        monochromatic: getHexColors(tinycolor(this.baseColor).monochromatic()),
+        tetrad: getHexColors(tinycolor(this.baseColor).tetrad()),
+        splitcomplement: getHexColors(
+          tinycolor(this.baseColor).splitcomplement()
+        ),
+        triad: getHexColors(tinycolor(this.baseColor).triad())
+      };
+    }
   }
 };
+
+function getHexColors(colors) {
+  return colors.map(function(color) {
+    return color.toHexString();
+  });
+}
 </script>
 
 <style>
