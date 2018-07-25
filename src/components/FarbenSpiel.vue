edab6f5772c9ecafb5afae24ac527b1b24fd7686
Mario Fink <mario.fink@goodyear.com>
2018-07-25 21:22:51 +0200

    add base color field

--- src/components/FarbenSpiel.vue
+++ src/components/FarbenSpiel.vue
@@ -1,11 +1,25 @@
 <template>
     <div>
         <h1>Farbenspiel</h1>
+        Base:
+        <input type="color" v-model="baseColor">
+        {{baseColor}}
     </div>
 </template>
 
 <script>
 export default {
-  name: "FarbenSpiel"
+  name: "FarbenSpiel",
+  data: () => {
+    return {
+      baseColor: "#c0ffee"
+    };
+  }
 };
 </script>
+
+<style>
+body {
+  font-family: sans-serif;
+}
+</style>
