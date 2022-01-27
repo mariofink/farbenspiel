efbaa5000e4293408c4b98e8440eb45018c5e184
Mario Fink <mario.fink@goodyear.com>
2022-01-27 22:38:43 +0100

    update packages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   lintOnSave: false,
-  baseUrl: process.env.NODE_ENV === "production" ? "/farbenspiel/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/farbenspiel/" : "/"
 };
